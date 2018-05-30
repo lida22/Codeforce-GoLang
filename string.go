a7f045c7cbb22aa078b3c29ce851c35a100b4782
Lida Joy <30574813+lida22@users.noreply.github.com>
2018-05-31 00:33:13 +0530

    Update string.go

--- string.go
+++ string.go
@@ -10,38 +10,17 @@ func main(){
 	fmt.Scan(&wrd)
 	word=strings.ToLower(wrd)
 	
-	//fmt.Println(word)
-	//array := [5]string{"a", "e", "i", "o", "u"}
-	//fmt.Println(array)
-	
-	s := []string{}//make([]string, len(word))
-	//s=append(s,".")
+	s := []string{}
 	for _,e1 := range word{
-
-	
-		
-		
 		if((string(e1)=="a") || (string(e1) =="e") || (string(e1)=="i") || (string(e1)=="o") || (string(e1)=="y") || (string(e1)=="u")){
 			continue
 		}else{
 			s=append(s,".")
 			s=append(s,string(e1))
-		}
-			
-		
+		}			
 	}
-	//fmt.Println(s)  
-	//for _, a := range s{
-	//	s=append(s,".")
-	//	s=append(s, a)
-	//}
-		
-		
 	result1 := strings.Join(s, "")
     	fmt.Println(result1)
-
-	
-
 }
 
 
