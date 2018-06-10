738629fb01c3c3586fcb52bef53e1eafd33afed4
Lida Joy <30574813+lida22@users.noreply.github.com>
2018-06-11 01:26:44 +0530

    Add files via upload

---
+++ vanya_cubes.go
@@ -0,0 +1,30 @@
+package main
+import(
+	"fmt"
+)
+func main(){
+	var a,k,p,ans int
+	fmt.Scan(&a)
+	k=0
+	p=0
+	f:=[]int{}
+	if(a==1){
+		fmt.Println("1")
+	}else{
+		for i:=1; i<=a+1; i++{
+			k=k+i
+			p=p+k
+			if(p==a){
+				ans=i
+			}
+			if(p>a){
+				f=append(f,i)
+				break
+			}
+		}
+		for _,j:=range(f){
+			ans=j-1}
+		fmt.Println(ans)
+	
+	}
+}
