62fada1cd06ffb2cce52e277fbdce2c9414b470c
Lida Joy <30574813+lida22@users.noreply.github.com>
2018-06-11 10:14:35 +0530

    Update vanya_cubes.go

--- vanya_cubes.go
+++ vanya_cubes.go
@@ -1,30 +1,34 @@
 package main
-import(
+
+import (
 	"fmt"
 )
-func main(){
-	var a,k,p,ans int
+
+func main() {
+	var a, k, p, ans int
 	fmt.Scan(&a)
-	k=0
-	p=0
-	f:=[]int{}
-	if(a==1){
+	k = 0
+	p = 0
+	f := []int{}
+	if a == 1 {
 		fmt.Println("1")
-	}else{
-		for i:=1; i<=a+1; i++{
-			k=k+i
-			p=p+k
-			if(p==a){
-				ans=i
+	} else {
+		for i := 1; i <= a+1; i++ {
+			k = k + i
+			p = p + k
+			if p == a {
+				ans = i
 			}
-			if(p>a){
-				f=append(f,i)
+			if p > a {
+				f = append(f, i)
 				break
 			}
 		}
-		for _,j:=range(f){
-			ans=j-1}
+		for _, j := range f {
+			ans = j - 1
+		}
 		fmt.Println(ans)
-	
+
 	}
 }
+
