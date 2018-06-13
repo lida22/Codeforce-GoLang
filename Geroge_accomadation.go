a96ed0b08688927d133ba6f08d4c271b7968585e
Lida Joy <30574813+lida22@users.noreply.github.com>
2018-06-13 21:01:33 +0530

    Add files via upload

---
+++ Geroge_accomadation.go
@@ -0,0 +1,34 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	var no, a, b, avail, tmp, sum int
+	fmt.Scan(&no)
+	avail = 0
+	tmp = 0
+	arr := []int{}
+	arr1 := []int{}
+	//total := []int{}
+	for i := 0; i < no; i++ {
+		fmt.Scan(&a, &b)
+		arr = append(arr, a)
+		arr1 = append(arr1, b)
+	}
+	for j := 0; j < len(arr); j++ {
+		if arr[j] == arr1[j] {
+			tmp = 0
+			//total = append(total, avail)
+		}
+		if arr[j] < arr1[j] {
+			if arr1[j] - arr[j] >= 2 {
+				avail = avail + 1
+				//total = append(total, avail)	
+			}
+		}
+	}
+	sum = tmp + avail
+	fmt.Println(sum)
+
+}
+
